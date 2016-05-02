99811fc2e80ba80f3d822cc084fcc151331c79b4
kautsiitd <kautsiitd@gmail.com>
2016-05-03 03:18:10 +0530

    Basic Results added

--- Src Code/Part 1.py
+++ Src Code/Part 1.py
@@ -10,6 +10,8 @@ from pybrain.datasets import ClassificationDataSet
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure.modules import SoftmaxLayer,TanhLayer
+import os
+import csv
 
 def load_dataset(dataset, X, y):
     enc = OneHotEncoder(n_values=10)
@@ -17,7 +19,8 @@ def load_dataset(dataset, X, y):
     for i in range(y.shape[0]):
         dataset.addSample(X[i, :], yenc[i][0])
 
-NUM_EPOCHS = 10
+LEARNING_RATE = .001
+NUM_EPOCHS = 50
 NUM_HIDDEN_UNITS = 25
 
 print "Loading MATLAB data..."    
@@ -26,19 +29,6 @@ X = data["data_image"]
 y = data["data_labels"]
 n_features = X.shape[1]
 n_classes = len(np.unique(y))
-
-# visualize data
-# get 100 rows of the input at random
-print "Visualize data..."
-idxs = np.random.randint(X.shape[0], size=100)
-fig, ax = plt.subplots(10, 10)
-img_size = math.sqrt(n_features)
-for i in range(10):
-    for j in range(10):
-        Xi = X[idxs[i * 10 + j], :].reshape(img_size, img_size).T
-        ax[i, j].set_axis_off()
-        ax[i, j].imshow(Xi, aspect="auto", cmap="gray")
-plt.show()
 
 # split up training data for cross validation
 print "Split data into training and test sets..."
@@ -55,11 +45,12 @@ print fnn
 
 # train network
 print "Training network..."
-trainer = BackpropTrainer(fnn, ds_train)
+trainer = BackpropTrainer(fnn, ds_train, learningrate=LEARNING_RATE)
 for i in range(NUM_EPOCHS):
     error = trainer.train()
     print "Epoch: %d, Error: %7.4f" % (i, error)
-    
+# error = trainer.trainUntilConvergence()
+
 # predict using test data
 print "Making predictions..."
 ypreds = []
@@ -67,5 +58,31 @@ ytrues = []
 for i in range(Xtest.shape[0]):
     pred = fnn.activate(Xtest[i, :])
     ypreds.append(pred.argmax())
-    ytrues.append(ytest[i])
+    ytrues.append(ytest[i][0])
 print "Accuracy on test set: %7.4f" % accuracy_score(ytrues, ypreds, normalize=True)
+
+c_label	  = [0]*10
+frqs	= [['Actual/Predict',0,1,2,3,4,5,6,7,8,9,'Recall']]+[[i]+[0 for j in range(10)] for i in range(10)]+[['Precision']]
+# saving results
+for i in range(Xtest.shape[0]):
+	dir_name = "../Output/3.b/"+str(ypreds[i])
+	try:
+		os.mkdir(dir_name)
+	except:pass
+	scipy.misc.imsave(dir_name+"/"+str(c_label[ypreds[i]])+".bmp",Xtest[i].reshape(28,28))
+	c_label[ypreds[i]]	+= 1
+	frqs[ypreds[i]+1][ytest[i]+1] += 1
+
+for i in range(1,11):
+	if sum(frqs[i]) != 0:
+		frqs[ i].append(round((frqs[i][i]*100.0)/sum(frqs[i]),2))
+	else:
+		frqs[ i].append('inf')
+	if sum(zip(*frqs[1:-1])[i]) != 0:
+		frqs[-1].append(round((frqs[i][i]*100.0)/sum(zip(*frqs[1:-1])[i]),2))
+	else:
+		frqs[-1].append('inf')
+
+with open("../Output/3.b/Result.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(frqs)
